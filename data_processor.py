30dd97a99ff4a42995220597e7bde5ccd3f816d6
TomasKopunec <tomas.kopunec5@gmail.com>
2024-07-07 18:52:55 +0100

    Added few more imports

--- data_processor.py
+++ data_processor.py
@@ -1,9 +1,9 @@
-# data_processor.py
-from utils.utils import validate_email
-
 """
 Contains the DataProcessor class which has methods for processing data and checking the validity of email addresses using the function from utils.py.
 """
+from utils.utils import validate_email
+
+
 class DataProcessor:
     def process_data(self, data):
         return [item.upper() for item in data]
